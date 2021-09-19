7f265f02b23b48dc668aa5ec6539bc45b3f8a49f
Petr Palowski <ppw@fiktivnimejl.com>
2021-09-19 13:26:41 +0200

    render game

--- src/Tetris.js
+++ src/Tetris.js
@@ -1,8 +1,15 @@
-import React, {useEffect, useReducer} from "react";
-import { TETROMINOS, rotate } from "./Tetrominos";
-import useInterval from "./useInterval"
+import React, {useEffect, useReducer, useRef} from "react";
 
-const SPEED =       1000 // [ms]
+const BRICKS = [      // 0 = nic/EMPTY , cislo = index barvy z COLORS. x a y rozmer pole kazde brick musi byt stejny
+  [ [1,1], [1,1] ],
+  [ [2,2,0], [0,2,2], [0,0,0] ],
+  [ [0,3,3], [3,3,0], [0,0,0] ],
+  [ [0,4,0], [4,4,4], [0,0,0] ],
+  [ [0,5,0], [0,5,0], [5,5,0] ],
+  [ [0,6,0,0], [0,6,0,0], [0,6,0,0], [0,6,0,0] ]
+]
+const COLOR =       ["#eee","blue", "red", "green", "yellow", "magenta","orange", "violet", "grey"]
+const SPEED =       300 // [ms]
 const EMPTY =       0
 const GAME_SIZE = {
   Width:            10,
@@ -16,140 +23,124 @@ const CONTROL = {
 }
 const ACTION = {
   Move:             "MOVE",     // pohyb doleva/doprava
-  StepFall:         "STEPFALL"  // pad kosticky z aktualni pozice az dolu
-}
-const COLOR = ["#eee","blue", "red", "green", "yellow", "magenta","orange", "violet", "grey"]
-
-let initialBoard = []
-for (let y=0; y<GAME_SIZE.Height; y++) {
-  initialBoard[y] = []
-  for (let x=0; x<GAME_SIZE.Width; x++) {
-    initialBoard[y][x] = EMPTY
-  }
+  TimeTick:         "TIMETICK"  // pad kosticky z aktualni pozice az dolu
 }
 const initialState = {
-  player: {
-    x: Math.floor( GAME_SIZE.Width / 2 ),
-    y: 0
+  position: {
+    x:              Math.floor( GAME_SIZE.Width / 2 ),
+    y:              0
   },
-  tetromino:  TETROMINOS[1], // TETROMINOS[ Math.floor(Math.random() * TETROMINOS.length) ] ,
-  board: initialBoard
+  brick:            BRICKS[ Math.floor(Math.random() * BRICKS.length) ],
+  board:            [...Array(GAME_SIZE.Height)].map(x=>Array(GAME_SIZE.Width).fill(EMPTY))    
 }
 
-function checkCollision(board, tetromino, player) {
-  // projdu kazdy jednotlivy bod prave hraneho tetromina
-  for( let y=0; y<tetromino.length; y++) {
-    for (let x=0; x<tetromino[y].length; x++) {
-      // kolize vyhodnocuju POUZE v pripade, ze jsem na kousku teromina. tj. nevyhodnucuju "vzduch" okolo
-      if (tetromino[y][x] !== 0) {
-        // 1. neprekroceni vodorovneho rozsahu (x)
-        if ( (x + player.x) < 0 || (x + player.x)>=GAME_SIZE.Width ) {
-          console.log("Kolize se svislym okrajem")
-          return true
-        }
-        // 2. neprekroceni spodni hrany
-        if ( (y + player.y)>=GAME_SIZE.Height ) {
-          console.log("Kolize se spodnim okrajem")
-          return true
-        }
-        // 3. naraz do jiz polozenych kosticek - kontrola obsahu plochy ZA AKTUALNIM tetrominem. pokud uz tam neco je, pak nastala kolize
-        if ( (board[y + player.y][x + player.x]) !== 0 ) {
-          console.log("kolize s jiz polozenou kostickou")
-          return true
-        }
-      }
-    }
-  }
+function checkCollision(board, brick, position) {
+  for( let y=0; y<brick.length; y++ )
+    for (let x=0; x<brick[y].length; x++ )
+      // kolize vyhodnocuju POUZE v pripade, ze jsem na kousku brick. tj. nevyhodnucuju "vzduch" okolo
+      if ((brick[y][x] !== 0) && ( 
+          // 1. neprekroceni vodorovneho rozsahu (x)
+          ( (x + position.x) < 0 || (x + position.x) >= GAME_SIZE.Width ) 
+          // 2. neprekroceni spodni hrany (y)
+          || ( (y + position.y) >= GAME_SIZE.Height ) 
+          // 3. naraz do jiz polozenych kosticek - kontrola obsahu plochy ZA AKTUALNIM tetrominem. pokud uz tam neco je, pak nastala kolize
+          || ( (board[y + position.y][x + position.x]) !== 0 ) 
+        )) return true
+}
+
+function rotateBrick(matrix) { 
+  let rotated = matrix[0].map((val, index) => matrix.map(row => row[index]).reverse())
+  return rotated
 }
 
 function gameReducer(state, action) {
   switch (action.type) {
-    // ovladani kosticky je mozne i mimo casovy interval posunu. lze lze vykonat vicero akci pri jednom pohybu kosticky dolu
     case ACTION.Move:
       switch (action.keyCode) {
         case CONTROL.Left: 
-          if (!checkCollision(state.board, state.tetromino, {x: state.player.x-1, y: state.player.y} )) 
-            return {...state, player: {x: state.player.x-1, y: state.player.y} }
-          break
+          if (!checkCollision(state.board, state.brick, {x: state.position.x-1, y: state.position.y} )) 
+            return {...state, position: {x: state.position.x-1, y: state.position.y} }
+          break  
         case CONTROL.Right: 
-          if (!checkCollision(state.board, state.tetromino, {x: state.player.x+1, y: state.player.y} )) 
-            return {...state, player: {x: state.player.x+1, y: state.player.y} }
+          if (!checkCollision(state.board, state.brick, {x: state.position.x+1, y: state.position.y} )) 
+            return {...state, position: {x: state.position.x+1, y: state.position.y} }
           break
         case CONTROL.Rotate: 
-          if (!checkCollision(state.board, rotate(state.tetromino), state.player )) 
-            return {...state, tetromino: rotate(state.tetromino) }
+          if (!checkCollision(state.board, rotateBrick(state.brick), state.position )) 
+            return {...state, brick: rotateBrick(state.brick) }
           break
         case CONTROL.Fall: 
+          // TODO
           break
+        default:
       }
       break
-    case ACTION.StepFall: 
-      let newY = state.player.y + 1
+    case ACTION.TimeTick: 
       // overim, jestli nasledna pozice neni v kolizi s jiz polozenymi kostickami
-      if (checkCollision( state.board, state.tetromino, {x: state.player.x, y: newY} ) ) {
-        // 1. tetromino presunout do boardu
+      let newY = state.position.y + 1
+      if (checkCollision( state.board, state.brick, {x: state.position.x, y: newY} ) ) {
+        // 1. brick presunout do boardu
         let newBoard = [...state.board]
-        for( let y=0; y<state.tetromino.length; y++) {
-          for (let x=0; x<state.tetromino[y].length; x++) {
-            if (state.tetromino[y][x] !== 0) 
-              newBoard[y + state.player.y][x + state.player.x] = state.tetromino[y][x]
-          }
-        }
+        for( let y=0; y<state.brick.length; y++) 
+          for (let x=0; x<state.brick[y].length; x++) 
+            if (state.brick[y][x] !== 0) 
+              newBoard[y + state.position.y][x + state.position.x] = state.brick[y][x]
         // 2. vymazat plne radky
         // 3. vyrobit nove teromino
-        return {
-          player: {
-            x: Math.floor( GAME_SIZE.Width / 2 ),
-            y: 0
-          },
-          tetromino:  TETROMINOS[ Math.floor(Math.random() * TETROMINOS.length) ] ,
-          board: newBoard
-        }
-
-
+        return { ... state, 
+          position: {x: Math.floor( GAME_SIZE.Width / 2 ), y: 0 }, 
+          brick: BRICKS[ Math.floor(Math.random() * BRICKS.length) ], 
+          board: newBoard}
       } else {        
         // nic nebrani v posunu na naslednou pozici
-        return {...state, player: {x: state.player.x, y: newY} }
+        return {...state, position: {x: state.position.x, y: newY} }
       }
-      break
+    default:
   }
   return {...state}
 }
 
-function Tetris() {
-  let [state, dispatch] = useReducer(gameReducer, initialState)
-  
-  useInterval( () => { dispatch({type: ACTION.StepFall}) }, SPEED)  
+function useInterval(callback, delay) {
+  const savedCallback = useRef(callback)
+  useEffect(() => { savedCallback.current = callback }, [callback])
+  useEffect(() => {
+    if (delay === null) return
+    const id = setInterval(() => savedCallback.current(), delay)
+    return () => clearInterval(id)
+  }, [delay])
+}
 
+function Box({color}) {
+  return (<div style={{ width:10, height:10, display: 'inline-block', backgroundColor: color, border: '1px solid #ccc'}} />)
+}
+
+export default function Tetris() {
+  let [state, dispatch] = useReducer(gameReducer, initialState)
+  useInterval(() => { dispatch({type: ACTION.TimeTick}) }, SPEED)  
+  useEffect(() => { document.addEventListener('keydown', handleGameAction); return () => { document.removeEventListener('keydown', handleGameAction) } });  
   console.log(state)
 
   function handleGameAction(e) {
     // pouze kdyz je zmacnuta sipka... a pocita se pouze 1.stisk sipky!
     if ( [CONTROL.Left, CONTROL.Right, CONTROL.Rotate, CONTROL.Fall].includes(e.keyCode) ) {
-      console.log("klik")
-      // potlaci default akci prohlizece 
       e.preventDefault(); 
-      // nastavi novy smer pohybu
       dispatch({type: ACTION.Move, keyCode: e.keyCode}) }
   }
 
-  useEffect(() => {
-    document.addEventListener('keydown', handleGameAction)
-    return () => { document.removeEventListener('keydown', handleGameAction) }
-  });  
-
-  return (<div className="Game">
-          {
-            // radky hraci plochy
-            [...Array(GAME_SIZE.Height).keys()].map( y => {
-              // vnejsi obal radku
-              return <div style={{display: 'block', lineHeight: 0}} key={y}>{ 
-                // sloupecek v kazdem z radku
-                [...Array(GAME_SIZE.Width).keys()].map( x => {
-                  // POKUD se na vykreslovane pozici nachazi jidlo...
-                    return <div  key={`${x}-${y}`} style={{ width:10, height:10, display: 'inline-block', backgroundColor: COLOR[state.board[y][x]],  border: '1px solid #ccc'}}/>})
-              }</div>})} 
-          </div>); 
+  let out = [], draw = []
+  for (let y=0; y<GAME_SIZE.Height;y++) {
+    draw[y] = []
+    for (let x=0;x<GAME_SIZE.Width;x++) {
+      // implicitne vezmu varvu z kostices jiz zafixovanych na desce
+      draw[y][x] = <Box color={COLOR[state.board[y][x]]} key={`${y}-${x}`}/>
+      // vykresleni aktualne padajici kosticky
+      for( let yTetro=0; yTetro<state.brick.length; yTetro++) 
+        for (let xTetro=0; xTetro<state.brick[yTetro].length; xTetro++)
+          // vyhodnocuju jen jedinou kosticku z prochazeneho tetromina - ta ktera je na pozici Y,X
+          if ( x === xTetro+state.position.x && y === yTetro+state.position.y && state.brick[yTetro][xTetro] !== 0) 
+            draw[y][x] = <Box color={COLOR[state.brick[yTetro][xTetro]]} key={`${y}-${x}`} />
+    }
+    out.push(<div style={{display: 'block', lineHeight: 0}} key={y}>{[...draw[y]]}</div>)
+  }
+  return (<div className="Game">{out}</div>); 
 }
-
-export default Tetris;
